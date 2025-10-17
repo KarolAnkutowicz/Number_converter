b3b1d8780668a9075fb366f21ed5fe593fb6df00
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2025-10-17 23:37:07 +0200

    Init commit

---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+﻿/********** BEGIN_OF_FILE **********/
+/*
+ * author: Karol Ankutowicz
+ * create: 17.10.2025
+ * file: main.cpp
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Number converter..." << std::endl;
+}
+
+/* main.cpp */
+/********** END_OF_FILE **********/
